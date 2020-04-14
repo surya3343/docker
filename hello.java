807be1b9eb27f9c3a9738e42c2bd0d6b8c5a5f0e
surya3343 <63227905+surya3343@users.noreply.github.com>
2020-04-14 18:34:10 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 import java.util.*;
 ​
-class Hello{  
+class hello{  
     public static void main(final String[] args) {
         System.out.println("This is java app \n by using Docker");  
     }  
